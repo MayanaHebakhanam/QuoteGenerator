06e7163c34ac64246621575218d0ac2a8b22416f
Mayana Heba Khanam <76441726+MayanaHebakhanam@users.noreply.github.com>
2023-10-27 12:10:55 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,38 +2,37 @@ import './App.css';
 import React, {useState} from 'react';
 
 const App = () => {
-  const url = "https://api.quotable.io/random";
-  let quoteData = {
-    content: "Let time be your only competitor.",
-    author: "Ahmed Saber"
+   
+  const url="https://api.quotable.io/random";
+  let quoteData={
+    content:"Let time be your only competitor.",
+    author:"Ahmed Saber"
   }
-  const [quote, setQuote] = useState(quoteData)
+  let [quote,setQuote]=useState(quoteData);
 
-  const generateQuote = () => {
+  const generateQuote=()=>{
     fetch(url)
-      .then(response => response.json())
-      .then(data => {
-        console.log(data)
-        setQuote(data)
-      });
+    .then((response)=>response.json())
+    .then((data)=>{
+      setQuote(data);
+    })
   }
 
-  const copy = () => {
-    navigator.clipboard.writeText(quote.author + " once said: " + quote.content)
-    alert('copied')
+  const copy=()=>{
+    navigator.clipboard.writeText(quote.author+" once said "+quote.content);
+    alert("Quote Copied..!");
   }
-
-  return (
+  return(
     <>
-      <h1>Quote Generator React App</h1>
-      <div className="container">
-        <p>{quote.content}</p>
-        <span>{quote.author}</span>
-        <div className="btns">
-          <button onClick={copy} className="btn">Copy</button>
-          <button onClick={generateQuote}>Generate Another Quote</button>
-        </div>
-      </div>
+     <h1>Quote Generator React App</h1>
+     <div className='container'>
+     <p>{quote.content}</p>
+     <span>{quote.author}</span>
+     <div className='btns'>
+     <button onClick={copy} >Copy</button>
+     <button onClick={generateQuote}>Generate Quote</button>
+     </div>
+     </div>
     </>
   )
 }
